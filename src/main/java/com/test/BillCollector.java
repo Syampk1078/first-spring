ac7c889c0b8830cf7857e6bae826a79cb4dd5eda
K Syam Prasad <K Syam Prasad@DESKTOP-69N137D>
2024-12-25 18:49:33 +0530

    Constructor Injection using xml config

--- src/main/java/com/test/BillCollector.java
+++ src/main/java/com/test/BillCollector.java
@@ -3,12 +3,20 @@ package com.test;
 public class BillCollector {
 	
 	private IPayment payment;
+
+	public void setPayment(IPayment payment) {
+		this.payment = payment;
+	}
+
+	public BillCollector() {}
 	
-	  public void setPayment(IPayment payment) {
-		  System.out.println("Called");
-	        this.payment = payment;
-	    }
-	
+	public BillCollector(IPayment payment) {
+		System.out.println("Constructor Executed");
+		this.payment = payment;
+	}
+
+
+
 	public void collectPayment(double amount) {
 		System.out.println(payment.getClass().getSimpleName());
 		String status = payment.pay(amount);
